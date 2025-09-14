e25c355dc2c878fec6b03216d3435bfb4f6e55cf
Ryankibet <ryankibet84@gmail.com>
2025-09-15 02:15:53 +0300

    make changes

--- proj_leadpage/settings.py
+++ proj_leadpage/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,7 +111,7 @@ USE_TZ = True
 ROOT_URLCONF = 'proj_leadpage.urls'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]   # add this line
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # where collectstatic will copy them
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
