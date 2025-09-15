9f096a99177ec68395c318685a6108bc2b7fe521
Ryankibet <ryankibet84@gmail.com>
2025-09-15 12:23:34 +0300

    make changes

--- proj_leadpage/settings.py
+++ proj_leadpage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c76u(^fffa9*b3^s93eyz&z@)92nz0=0ewr!_r2=jel1egyspc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
